8fc666c51290018f11acaf8429a01dbf5f43cc14
Kshira Saagar <kshira.saagar@gmail.com>
2021-01-28 22:47:52 +0000

    Commiting from a Manifest

---
+++ explaining_factors/Explaining_Factors.R
@@ -0,0 +1,20 @@
+
+#Explaining Factors
+
+setwd("~/all_the_r/explaining_factors")
+# Continuous to Categorical - for Univariate/Models
+a <- sample(1:50,10)
+a
+cut(a,3)
+cut(a,3,labels = c("Low","Medium","High"))
+
+# Storing Huge Strings in a Compatible Form - for Models
+a <- c("Monday","Friday","Saturday","Monday","Sunday","Tuesday","Tuesday","Friday")
+a
+factor(a)
+factor(a, levels = c("Monday","Tuesday","Saturday","Sunday"))
+factor(a, levels = c("Monday","Tuesday","Saturday","Sunday"),labels = c("Mon","Tue","Sat","Sun"))
+
+print(a)
+print("It runs!")
+
